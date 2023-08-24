fdac8b043160a930b8e92854e68d81211df8bcb1
Moffat <moffat.muga@gmail.com>
2023-08-24 12:56:42 +0300

    created hero_powers routes

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,24 @@
 class HeroPowersController < ApplicationController
+
+    def create
+        hero_power = HeroPower.new(hero_power_params)
+    
+        if hero_power.save
+          hero = hero_power.hero
+          render json: {
+            id: hero.id,
+            name: hero.name,
+            super_name: hero.super_name,
+            powers: hero.powers
+          }
+        else
+          render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    
+      private
+    
+      def hero_power_params
+        params.require(:hero_power).permit(:strength, :power_id, :hero_id)
+      end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,50 @@ power1 = Power.create(name:"super strength", description:"gives the wielder supe
 power2 = Power.create(name:"flight", description:"gives the wielder the ability to fly through the skies at supersonic speed")
 
 puts "seeding done ...."
+
+powers = [
+    {
+      name: 'super strength',
+      description: 'gives the wielder super-human strengths'
+    },
+    {
+      name: 'flight',
+      description: 'gives the wielder the ability to fly through the skies at supersonic speed'
+    }
+  ]
+  
+  powers.each do |power|
+    Power.create(power)
+  end
+
+  heroes = [
+    {
+      name: 'Kamala Khan',
+      super_name: 'Ms. Marvel'
+    },
+    {
+      name: 'Peter Parker',
+      super_name: 'Spider-Man'
+    }
+  ]
+  
+  heroes.each do |hero|
+    Hero.create(hero)
+  end
+
+  hero_powers = [
+    {
+      strength: 'Average',
+      power_id: 1,
+      hero_id: 1
+    },
+    {
+      strength: 'Strong',
+      power_id: 2,
+      hero_id: 2
+    }
+  ]
+  
+  hero_powers.each do |hero_power|
+    HeroPower.create(hero_power)
+  end
