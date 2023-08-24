c7e2e39372439e1046eb14ff70c1c585f17af973
Moffat <moffat.muga@gmail.com>
2023-08-24 11:48:57 +0300

    created routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ hero2 = Hero.create(name:"Doreen Green", super_name: "Squirrel Girl")
 hero3 = Hero.create(name:"Gwen Stacy", super_name: "Spider-Gwen")
 
 puts "seeding done...."
+
+power1 = Power.create(name:"super strength", description:"gives the wielder super-human strengths")
+power2 = Power.create(name:"flight", description:"gives the wielder the ability to fly through the skies at supersonic speed")
+
+puts "seeding done ...."
