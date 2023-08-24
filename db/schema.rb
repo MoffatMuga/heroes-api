bb28e6abb1fde22f646bbae2a97bc9f497ae1d93
Moffat <moffat.muga@gmail.com>
2023-08-24 10:20:13 +0300

    created relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_070609) do
+  create_table "hero_powers", force: :cascade do |t|
+    t.integer "hero_id"
+    t.integer "power_id"
+    t.string "strength"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "super_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "powers", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
